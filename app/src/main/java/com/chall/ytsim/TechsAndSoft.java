fa1e5b0ba8f04a627ac7fd603b92c785af08bc5a
Ded Banzai <dedbanzai3@yandex.ru>
2017-08-12 14:07:34 +0300

    VideoComm

--- app/src/main/java/com/chall/ytsim/TechsAndSoft.java
+++ app/src/main/java/com/chall/ytsim/TechsAndSoft.java
@@ -1,8 +1,64 @@
 package com.chall.ytsim;
 
+import java.util.List;
+
 /**
  * Created by КапляНикотина on 12.08.2017.
  */
 
 public class TechsAndSoft {
+   private BaseTech camera;
+   private BaseTech computer;
+   private BaseTech microphone;
+   private BaseTech softMusic;
+   private BaseTech softVideo;
+
+    public TechsAndSoft(BaseTech camera, BaseTech computer, BaseTech microphone, BaseTech softMusic, BaseTech softVideo) {
+        this.camera = camera;
+        this.computer = computer;
+        this.microphone = microphone;
+        this.softMusic = softMusic;
+        this.softVideo = softVideo;
+    }
+
+
+    public BaseTech getCamera() {
+        return camera;
+    }
+
+    public void setCamera(BaseTech camera) {
+        this.camera = camera;
+    }
+
+    public BaseTech getComputer() {
+        return computer;
+    }
+
+    public void setComputer(BaseTech computer) {
+        this.computer = computer;
+    }
+
+    public BaseTech getMicrophone() {
+        return microphone;
+    }
+
+    public void setMicrophone(BaseTech microphone) {
+        this.microphone = microphone;
+    }
+
+    public BaseTech getSoftMusic() {
+        return softMusic;
+    }
+
+    public void setSoftMusic(BaseTech softMusic) {
+        this.softMusic = softMusic;
+    }
+
+    public BaseTech getSoftVideo() {
+        return softVideo;
+    }
+
+    public void setSoftVideo(BaseTech softVideo) {
+        this.softVideo = softVideo;
+    }
 }
--- app/src/main/java/com/chall/ytsim/YouTubeAd.java
+++ app/src/main/java/com/chall/ytsim/YouTubeAd.java
@@ -5,4 +5,24 @@ package com.chall.ytsim;
  */
 
 public class YouTubeAd {
+    private final double StartSeparateWatchIncome = 0.002; //dollars
+    private final double CanalCountCoeff = 1000; //test
+
+    public double getSeparateWatchIncome() {
+        return SeparateWatchIncome;
+    }
+
+    public void setSeparateWatchIncome(double separateWatchIncome) {
+        SeparateWatchIncome = separateWatchIncome;
+    }
+
+    private double SeparateWatchIncome;
+
+    public YouTubeAd(int canalWatchCount) {
+        //computeIncome(canalWatchCount); тут ещё надо подумать
+    }
+
+    public void computeIncome(int canalWatchCount) {
+        SeparateWatchIncome = StartSeparateWatchIncome+canalWatchCount/CanalCountCoeff;
+    }
 }
