7199367113eb8e42d03859e3be7da0ee07053dd5
Ded Banzai <dedbanzai3@yandex.ru>
2017-08-12 15:28:10 +0300

    VideoComm

--- app/src/main/java/com/chall/ytsim/Utilites.java
+++ app/src/main/java/com/chall/ytsim/Utilites.java
@@ -10,10 +10,14 @@ import java.util.List;
 public class Utilites {
 
     List<BaseTech> comps;
+    List<BaseTech> cameras;
+    List<BaseTech> softVideo;
 
     public Utilites()
     {
         setComps();
+        setCameras();
+        setSoftVideo();
     }
 
     void setComps()
@@ -22,8 +26,33 @@ public class Utilites {
         comps.add(new BaseTech("NextFuture",70,1));
     }
 
+    void setCameras()
+    {
+        cameras = new ArrayList<BaseTech>();
+        cameras.add(new BaseTech("Sany1604",30,1.6));
+        cameras.add(new BaseTech("PensionicSD-56",50,2.0));
+    }
+
+    void setSoftVideo()
+    {
+        softVideo = new ArrayList<BaseTech>();
+        softVideo.add(new BaseTech("SanyVegos", 30,1.5));
+    }
+
     BaseTech getComps(int index)
     {
         return comps.get(index);
     }
+
+    BaseTech getCameras(int index)
+    {
+        return cameras.get(index);
+    }
+
+    BaseTech getSoftVideo(int index)
+    {
+        return softVideo.get(index);
+    }
+
+
 }
