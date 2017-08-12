f404e7dc9f491223c93e38852e3dfd983911440f
chall2 <chall4@yandex.ru>
2017-08-12 12:41:58 +0300

    ver3

--- app/src/main/java/com/chall/ytsim/MainActivity.java
+++ app/src/main/java/com/chall/ytsim/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //EldarComment
+        //f4534d
     }
 }
