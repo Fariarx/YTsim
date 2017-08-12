a3c96ea36205c528189dd7b5a654193e2a773bdb
chall2 <chall4@yandex.ru>
2017-08-12 13:03:33 +0300

    ver3

---
+++ app/src/main/java/com/chall/ytsim/Canal.java
@@ -0,0 +1,17 @@
+package com.chall.ytsim;
+
+import android.graphics.Bitmap;
+
+import java.util.List;
+
+/**
+ * Created by Chall on 12.08.2017.
+ */
+
+public class Canal {
+    String nameCanal;
+    Bitmap iconCanal;
+    int ratingCanal;
+    List<Video> ListVideo;
+
+}
